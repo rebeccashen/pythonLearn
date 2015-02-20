3360bfa570fa53eea779877ccc5815ab67de2753
rebeccayshen <rebecca@emenar.com>
2015-02-20 15:43:43 -0800

    python try 5

--- testserver.py
+++ testserver.py
@@ -1,15 +1,14 @@
-__author__ = 'rebeccashen'
 
 #!/usr/bin/env python
 # -*- coding: UTF-8 -*-
 
-
-#print "Content-Type: text/html"
-#print
+print "Content-Type: text/html\n\n"
+print
 print """\
 <html>
 <body>
 <h2>Hello World!</h2>
+<h3>Kaboom!!</h3>
 </body>
 </html>
 """
